930b1ef624554cdb3f7b67fc666bef25bd30e1de
shivayanbora123 <preetiyadav.kit@gmail.com>
2019-10-28 02:20:51 +0530

    updated file location

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 import ProductList from "./product-list/product-list";
 
 const productList = new ProductList();
-productList.init("./data.json");
+productList.init("../data.json");
 
 export default productList;
